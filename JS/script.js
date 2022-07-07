efe6587b7e758b062932b3602931576907c5b88c
s-kvng <105977478+s-kvng@users.noreply.github.com>
2022-07-07 18:08:11 +0000

    Update 4
    added the hours and mins numbers

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,21 @@
 const selectMenu = document.querySelectorAll('select');
 
-//console.log(selectMenu);  
+console.log(selectMenu);  
 
-for(i = 12, i > 0,i--){
-    i = i < 10 ? "0" + i : i
+for (i = 12; i > 0;i--) {
+    i = i < 10 ? "0" + i : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend",option);
+}
+
+for (i = 59; i > 0 ; i--) {
+    i = i < 10 ? "0" + i : i;
+    let option =  `<option value=${i}>${i}</option>`;
+    selectMenu[1].firstElementChild.insertAdjacentHTML('afterend', option)
+}
+
+for (i = 2; i > 0 ; i--) {
+    let ampm = i == 1 ? "AM" : "PM";
+    let option = `<option value=${ampm}>${ampm}</option>`;
+    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
